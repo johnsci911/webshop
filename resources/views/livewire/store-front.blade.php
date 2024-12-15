d5fde3fe3435ecb292603e45e651dc4105f08926
jkcachero <johnguitarizta@gmail.com>
2024-12-16 04:44:32 +0800

    Money formatting

--- resources/views/livewire/store-front.blade.php
+++ resources/views/livewire/store-front.blade.php
@@ -1,9 +1,9 @@
 <div class="grid grid-cols-4 gap-4 mt-12">
     @foreach($this->products as $product)
-        <div>
-            <img style="height: 200px; width: auto;" src="{{ $product->image->path }}" alt="">
-            <h2 class="font-medium text-lg">{{ $product->name }}</h2>
-            <span class="text-gray-700 text-sm">{{ $product->price }}</span></span>
-        </div>
+    <div class="bg-white rounded-lg shadow p-4 flex flex-col items-center justify-center">
+        <img style="height: 200px; width: auto;" src="{{ $product->image->path }}" alt="">
+        <h2 class="font-medium text-lg text-left w-full">{{ $product->name }}</h2>
+        <span class="text-gray-700 text-sm text-left w-full">{{ $product->price }}</span>
+    </div>
     @endforeach
 </div>
