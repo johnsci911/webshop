61140959afddedd48c47762635be40539849263e
jkcachero <johnguitarizta@gmail.com>
2024-12-29 11:12:27 +0800

    Migrate session cart into a user's cart

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,17 @@
 
 namespace App\Providers;
 
+use App\Actions\Webshop\MigrateSessionCart;
+use App\Factories\CartFactory;
+use App\Models\User;
+use Exception;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Fortify\Fortify;
+use Log;
 use Money\Currencies\ISOCurrencies;
 use Money\Formatter\IntlMoneyFormatter;
 use Money\Money;
@@ -26,7 +34,24 @@ class AppServiceProvider extends ServiceProvider
     {
         Model::unguard();
 
-        Blade::stringable(function(Money $money) {
+        Fortify::authenticateUsing(function (Request $request) {
+            $user = User::where('email', $request->email)->first();
+
+            if ($user && Hash::check($request->password, $user->password)) {
+                try {
+                    $cart = $user->cart ?? $user->cart()->create();
+                    (new MigrateSessionCart)->migrate(CartFactory::make(), $cart);
+                    return $user;
+                } catch (Exception $e) {
+                    Log::error('Failed to migrate cart: ' . $e->getMessage());
+                    return $user;
+                }
+            }
+
+            return null;
+        });
+
+        Blade::stringable(function (Money $money) {
             $currencies = new ISOCurrencies();
             $numberFormatter = new \NumberFormatter('en_US', \NumberFormatter::CURRENCY);
             $moneyFormatter = new IntlMoneyFormatter($numberFormatter, $currencies);
